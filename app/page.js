39851429dc60de69487a237032f8192145fbd8db
Charmant Gatera <charmantgatera@Charmants-MacBook-Pro.local>
2023-11-27 17:14:56 +0200

    Added authentication

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import RegistrationPage from "./register/page"
 
 export default function Home() {
   return (
-    <div className="grid place-items-center bg-blue-400 h-[100vh]">
+    <div className="grid place-items-center h-[100vh]">
       <RegistrationPage />
     </div>
   )
