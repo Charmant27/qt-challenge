04fbc58f2d89ee4f4eade99a7d72b0fd41025772
Charmant Gatera <charmantgatera@Charmants-MacBook-Pro.local>
2023-11-28 11:46:22 +0200

    Added login and the tasks layout

--- components/Login.jsx
+++ components/Login.jsx
@@ -2,29 +2,28 @@
 import { useState } from "react"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
+import { signIn } from "next-auth/react"
 
 const Login = () => {
-    const [user, setUser] = useState({
-        email: '',
-        password: ''
-    }
-    )
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
     const router = useRouter()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
         try {
-            const res = await fetch('/api/login', {
-                method: 'POST',
-                body: JSON.stringify({
-                    email: user.email,
-                    password: user.password
-                })
+            const res = await signIn("credentials", {
+                email,
+                password,
+                redirect: false
             })
-            if (res.ok) {
-                router.push('/')
+            if(res.error) {
+                setErrorMessage('Invalid credentials')
+                return
             }
+            router.replace('/create-task')
         } catch (error) {
             console.log(error)
         }
@@ -34,13 +33,17 @@ const Login = () => {
             <div className="pb-8">
                 <h1 className="text-blue-400 text-2xl font-bold">Task Manager</h1>
             </div>
+            {errorMessage && (
+                <div className="pb-5">
+                    <h3 className="text-red-500 font-semibold text-lg">{errorMessage}</h3>
+                </div>
+            )}
             <form className="flex flex-col gap-12" onSubmit={handleSubmit}>
 
                 <input
                     type="email"
                     name="email"
-                    value={user.email}
-                    onChange={(e) => setUser({ ...user, email: e.target.value })}
+                    onChange={(e) => setEmail(e.target.value)}
                     placeholder="Email"
                     className="w-full border border-slate-200 px-6 py-3 rounded-md"
 
@@ -49,8 +52,7 @@ const Login = () => {
                 <input
                     type="password"
                     name="password"
-                    value={user.password}
-                    onChange={(e) => setUser({ ...user, password: e.target.value })}
+                    onChange={(e) => setPassword(e.target.value)}
                     placeholder="Your password"
                     className="w-full border border-slate-200 px-6 py-3 rounded-md"
 
---
+++ constants/index.js
@@ -0,0 +1,63 @@
+export const employees = [
+    {
+        id: 1,
+        name: 'Vital Gatera'
+    },
+
+    {
+        id: 2,
+        name: 'LeBron James'
+    },
+
+    {
+        id: 3,
+        name: 'Stephen Curry'
+    },
+
+    {
+        id: 4,
+        name: 'Lionel Messi'
+    },
+
+    {
+        id: 5,
+        name: 'David Villa'
+    },
+
+    {
+        id: 6,
+        name: 'Damian Lillard'
+    }
+]
+
+export const projects = [
+    {
+        id: 1,
+        name: 'E-wallet'
+    },
+
+    {
+        id: 2,
+        name: 'Tap & Go'
+    },
+
+    {
+        id: 3,
+        name: 'e-procurement system'
+    },
+
+    {
+        id: 4,
+        name: 'pay as you park system'
+    },
+
+    {
+        id: 5,
+        name: 'Uber drive mobile app'
+    },
+
+    {
+        id: 6,
+        name: 'e-school pay'
+    },
+]
